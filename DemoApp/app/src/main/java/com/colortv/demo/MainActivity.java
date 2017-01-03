5041e60504b433979020916b60a15e12429e5800
Marcin Urbańczyk <mar.urbanczyk@gmail.com>
2017-01-03 14:20:27 +0100

    Add Content Recommendation and update gradle

--- DemoApp/app/src/main/java/com/colortv/demo/MainActivity.java
+++ DemoApp/app/src/main/java/com/colortv/demo/MainActivity.java
@@ -8,10 +8,12 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.colortv.android.ColorTvAdListener;
+import com.colortv.android.ColorTvContentRecommendationListener;
 import com.colortv.android.ColorTvError;
 import com.colortv.android.ColorTvSdk;
 import com.colortv.android.OnCurrencyEarnedListener;
 import com.colortv.android.Placements;
+import com.colortv.android.TrackingEventType;
 
 public class MainActivity extends Activity {
 
@@ -39,6 +41,44 @@ public class MainActivity extends Activity {
         }
     };
 
+    private ColorTvContentRecommendationListener loadContentRecommendationListener = new ColorTvContentRecommendationListener() {
+
+        @Override
+        public void onLoaded(String placement) {
+            ColorTvSdk.showContentRecommendation(placement);
+        }
+
+        @Override
+        public void onError(String placement, ColorTvError error) {
+            Log.e(MainActivity.class.getSimpleName(), "Error while fetching ContentRecommendation for placement " +
+                    placement + " - " + error.getErrorCode().name() + ": " + error.getErrorMessage());
+        }
+
+        @Override
+        public void onClosed(String placement) {
+            Log.d(MainActivity.class.getSimpleName(), "ContentRecommendation has closed for placement: " + placement);
+        }
+
+        @Override
+        public void onExpired(String placement) {
+            Log.d(MainActivity.class.getSimpleName(), "ContentRecommendation has expired for placement: " + placement);
+        }
+
+        @Override
+        public void onContentChosen(String videoId, String videoUrl) {
+            super.onContentChosen(videoId, videoUrl);
+            /**
+             * User clicked element on content recommendation list
+             * You can start your video player activity here, with passed videoUrl
+             *
+             * Intent intent = new Intent(MainActivity.this, VideoActivity.class);
+             * intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+             * intent.putExtra(VideoActivity.VIDEO_URL, videoUrl);
+             * startActivity(intent);
+             */
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,6 +97,7 @@ public class MainActivity extends Activity {
         });
 
         ColorTvSdk.registerAdListener(loadAdListener);
+        ColorTvSdk.registerContentRecommendationListener(loadContentRecommendationListener);
 
         ColorTvSdk.onCreate();
     }
@@ -67,6 +108,7 @@ public class MainActivity extends Activity {
         Button showAppFeatureButton= (Button) findViewById(R.id.btnShowAppFeature);
         Button showDirectEngagementButton = (Button) findViewById(R.id.btnShowDirectEngagement);
         Button showVideoButton = (Button) findViewById(R.id.btnShowVideo);
+        Button showContentRecommendationButton = (Button) findViewById(R.id.btnShowContentRecommendation);
 
         View.OnClickListener buttonClickListener = new View.OnClickListener() {
             @Override
@@ -88,6 +130,10 @@ public class MainActivity extends Activity {
                     case R.id.btnShowVideo:
                         ColorTvSdk.loadAd(Placements.BETWEEN_LEVELS);
                         break;
+                    case R.id.btnShowContentRecommendation:
+                        ColorTvSdk.reportVideoTrackingEvent("1", TrackingEventType.VIDEO_COMPLETED, 342);
+                        ColorTvSdk.loadContentRecommendation("TestContent");
+                        break;
                 }
 
             }
@@ -98,6 +144,7 @@ public class MainActivity extends Activity {
         showAppFeatureButton.setOnClickListener(buttonClickListener);
         showDirectEngagementButton.setOnClickListener(buttonClickListener);
         showVideoButton.setOnClickListener(buttonClickListener);
+        showContentRecommendationButton.setOnClickListener(buttonClickListener);
     }
 
     @Override
