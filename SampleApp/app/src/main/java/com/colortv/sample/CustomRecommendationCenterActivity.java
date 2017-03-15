43a3f78644e272cacf147ac2baa67678673e90b0
Marcin Urbańczyk <mar.urbanczyk@gmail.com>
2017-03-15 16:55:58 +0100

    AS-435 Add two custom layouts

--- SampleApp/app/src/main/java/com/colortv/sample/CustomRecommendationCenterActivity.java
+++ SampleApp/app/src/main/java/com/colortv/sample/CustomRecommendationCenterActivity.java
@@ -20,16 +20,49 @@ public class CustomRecommendationCenterActivity extends ExoPlayerVideoActivity {
     private void setRecommendationCenterConfiguration() {
         recommendationConfig.resetToDefault();
 
-        recommendationConfig.setItemLayout(Device.TV, R.layout.custom_item_layout);
+        recommendationConfig.setItemLayout(Device.TV, R.layout.custom_yt_item_layout);
+        recommendationConfig.setGridLayout(Device.TV, R.layout.custom_mobile_grid_layout);
+        recommendationConfig.setRowCount(Device.TV, 3);
+
+        recommendationConfig.setItemLayout(Device.MOBILE, R.layout.custom_mobile_yt_item_layout);
+        recommendationConfig.setGridLayout(Device.MOBILE, R.layout.custom_mobile_grid_layout);
+        recommendationConfig.setSnapEnabled(false);
+
+        recommendationConfig.setItemLayout(Device.TABLET, R.layout.custom_mobile_yt_item_layout);
+        recommendationConfig.setGridLayout(Device.TABLET, R.layout.custom_mobile_grid_layout);
+        recommendationConfig.setRowCount(Device.TABLET, 1);
+
+        /*
+        --------------------------------------------------------------------------------------------
+        OTHER COMBINATIONS
+
+        recommendationConfig.setItemLayout(Device.TV, R.layout.custom_item_layout); // or custom_item_layout_2
         recommendationConfig.setGridLayout(Device.TV, R.layout.custom_grid_layout);
         recommendationConfig.setRowCount(Device.TV, 1);
 
         recommendationConfig.setItemLayout(Device.MOBILE, R.layout.custom_mobile_item_layout);
         recommendationConfig.setGridLayout(Device.MOBILE, R.layout.custom_mobile_grid_layout);
+        recommendationConfig.setSnapEnabled(true);
 
-        recommendationConfig.setItemLayout(Device.TABLET, R.layout.custom_item_layout);
+        recommendationConfig.setItemLayout(Device.TABLET, R.layout.custom_mobile_item_layout);
         recommendationConfig.setGridLayout(Device.TABLET, R.layout.custom_mobile_grid_layout);
-        recommendationConfig.setRowCount(Device.TABLET, 1);
+        recommendationConfig.setRowCount(Device.TABLET, 2);
+
+        --------------------------------------------------------------------------------------------
+        DEFAULT
+
+        recommendationConfig.setItemLayout(Device.TV, R.layout.colortv_default_item_layout); // or colortv_default_simplified_item_layout
+        recommendationConfig.setGridLayout(Device.TV, R.layout.colortv_default_grid_layout);
+        recommendationConfig.setRowCount(Device.TV, 2);
+
+        recommendationConfig.setItemLayout(Device.MOBILE, R.layout.colortv_default_mobile_item_layout);
+        recommendationConfig.setGridLayout(Device.MOBILE, R.layout.colortv_default_mobile_grid_layout);
+        recommendationConfig.setSnapEnabled(true);
+
+        recommendationConfig.setItemLayout(Device.TABLET, R.layout.colortv_default_mobile_item_layout);
+        recommendationConfig.setGridLayout(Device.TABLET, R.layout.colortv_default_mobile_grid_layout);
+        recommendationConfig.setRowCount(Device.TABLET, 2);
+        */
     }
 
 }
